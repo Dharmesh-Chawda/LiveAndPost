fd7b0231547165003f800d2351541bca813c360b
darshansatra1 <darshansatra1@gmail.com>
2020-11-26 12:23:52 +0530

    added sql file

--- live_and_post.sql
+++ live_and_post.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2020 at 08:19 AM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.4.9
+-- Generation Time: Nov 26, 2020 at 07:53 AM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,82 @@ SET time_zone = "+00:00";
 --
 -- Database: `live_and_post`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(100) NOT NULL,
+  `post_id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `comment` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(100) NOT NULL,
+  `post_id` int(100) NOT NULL,
+  `user_id` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `post_id`, `user_id`) VALUES
+(5, 3, 1),
+(6, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `id` int(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `description` varchar(2000) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `featured_image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`id`, `title`, `description`, `category`, `featured_image`) VALUES
+(3, 'Food', 'Food, substance consisting essentially of protein, carbohydrate, fat, and other nutrients used in the body of an organism to sustain growth and vital processes and to furnish energy. The absorption and utilization of food by the body is fundamental to nutrition and is facilitated by digestion. Plants, which convert solar energy to food by photosynthesis, are the primary food source. Animals that feed on plants often serve as sources of food for other animals. To learn more about the sequence of transfers of matter and energy in the form of food from organism to organism, see food chain.                                ', 'Enterntainment', 'http://localhost/liveandpost/assets/featuredImages/food.jfif');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(100) NOT NULL,
+  `profession` varchar(100) NOT NULL,
+  `avatar` varchar(100) NOT NULL,
+  `user_id` int(100) NOT NULL,
+  `user_role` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `profession`, `avatar`, `user_id`, `user_role`) VALUES
+(1, 'PHP', 'http://localhost/liveandpost/assets/uploads/man-avatar-profile-vector-21372076.jpg', 1, 1),
+(2, 'Flutter Developer', 'http://localhost/liveandpost/assets/uploads/a65832155622ac173337874f02b218fb.png', 2, 0);
 
 -- --------------------------------------------------------
 
@@ -40,15 +116,36 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `email`, `password`, `user_role`) VALUES
-(1, 'Dashan', 'darshansatra1@gmail.com', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 0),
-(3, 'darshan', 'darshansatar1@c.com', 'fd8ccb2f9b293f120b2a14e63b9d232832a7a4ee', 1),
-(4, 'sdfsdf', 'd@g.com', 'cfd7585fe30db8a6690cb4425b94fbaeaeceb483', 1),
-(5, 'Shruti', 'shruti@gmail.com', 'a1f239cbcd40f722555acfc7d23be06dee9d815e', 1),
-(6, 'Shruti', 'shruti@gmail.com', 'e9a08769c9578b42be52de2bafcaad0fb0e91191', 1);
+(1, 'darshan', 'darshansatra1@gmail.com', '8cb2237d0679ca88db6464eac60da96345513964', 1),
+(2, 'darshansatra1', 'darshan.ss@somaiya.edu', '8cb2237d0679ca88db6464eac60da96345513964', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,10 +158,34 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `profile`
+--
+ALTER TABLE `profile`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
